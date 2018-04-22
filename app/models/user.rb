6b92c649ca4818311303ddb8dbdc3851659374d5
Kakiuchi Shusuke <kakiuchis@gmail.com>
2018-04-22 23:11:51 +0900

    set sendgrid and devise confirm

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   enum role: { admin: 1, member: 2 }
 end
