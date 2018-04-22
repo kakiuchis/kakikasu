d7045b47ebb0f477e606b5d00da17f5adae1cba4
Kakiuchi Shusuke <kakiuchis@gmail.com>
2018-04-23 01:45:46 +0900

    add inquiry responce

---
+++ app/mailers/notice_mailer.rb
@@ -0,0 +1,18 @@
+class NoticeMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notice_mailer.sendmail_inquiry.subject
+  #
+  default from: ENV['SERVICE_ADDRESS']
+
+  def sendmail_inquiry(inquiry)
+    @inquiry = inquiry
+
+    mail to: inquiry.email,
+      bcc: User.where(role: "admin").pluck(:email),
+            subject: "【カキカス】お問い合わせありがとうございます！"
+        end
+      end
+      )
