f552fc9dbdbb9d370ac1c1a587a111f283768430
Kakiuchi Shusuke <kakiuchis@gmail.com>
2018-05-26 21:28:02 +0900

    make top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
   resources :announcements
   get 'users/index'
 
   resources :inquiries
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  root to: 'texts#index'
+  root 'top#index'
   resources :texts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
