4c1193070f4a985ae3ff4b3a9bc9948c41ade7b2
Kakiuchi Shusuke <kakiuchis@gmail.com>
2018-05-27 17:31:38 +0900

    texts index design change

--- app/helpers/texts_helper.rb
+++ app/helpers/texts_helper.rb
@@ -1,2 +1,9 @@
 module TextsHelper
+  def status_label(text)
+  	if Time.now - text.created_at < 60*60*24*7
+  	  content_tag(:span, 'NEW', class: "label label-warning")
+  	elsif Time.now - text.updated_at < 60*60*24*7
+  	  content_tag(:span, 'UPDATE', class: "label label-info")
+    end
+  end
 end
